7342325412a605530c476da2046868088ce089a6
RavenbornIV <41174251+RavenbornIV@users.noreply.github.com>
2018-08-07 10:11:53 -0300

    Update da Compra
    Adicionei mais algumas coisas, como uma variável "esgotado" que serve como flag pra caso o produto em questão esteja esgotado (o que vai ser mostrado ao lado da opção) e MESMO ASSIM o cliente escolha esse produto, a mensagem que vai aparecer é a de produto esgotado, e não de comando inválido. Adicionei também a parte de escolha de pão e de molho.

--- compra_sanduiche.c
+++ compra_sanduiche.c
@@ -1,6 +1,6 @@
 void compra_sanduiche(void)
 {
-  int ok = 0, escolha, tam = 10;
+  int ok = 0, escolha, esgotado = 0, tam = 10;
   float somatorio;
   Sanduiche sanduiche;
   Produto produtos[10];
@@ -37,7 +37,14 @@ void compra_sanduiche(void)
   {
     if(produtos[i].tipo == 1)
     { 
-      printf("%d - %s\n", produtos[i].id, produtos[i].nome);
+      printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+      if(produtos[i].qtd <= 0)
+      {
+        printf(" (Esgotado)");
+      }
+
+      printf("\n");
     }
   }
 
@@ -52,16 +59,157 @@ void compra_sanduiche(void)
       {
         if(escolha == produtos[i].id)
         {
-          sanduiche.recheio = produtos[i];
-          ok = 1;
-          break;
+          if(produtos[i].qtd > 0)
+          {
+            sanduiche.recheio = produtos[i];
+            ok = 1;
+            break;
+          }
+          else
+          {
+            esgotado = 1;
+          }
         }
       }
     }
 
     if(ok == 0)
     {
-      printf("\nComando inválido, por favor insira um dos números indicados.\n\n");
+      if(esgotado == 1)
+      {
+        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+        esgotado = 0;
+      }
+      else
+      {
+        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+      }
+    }
+    else if(ok == 1)
+    {
+      ok = 0;
+      break;
+    }
+  }
+
+  printf("\t\n\nTERCEIRO PASSO:\n");
+  printf("Escolha o tipo de pão do seu sanduíche!");
+
+  //Impressão das Opções de Pão
+  for(int i = 0; i < tam; i++)
+  {
+    if(produtos[i].tipo == 2)
+    { 
+      printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+      if(produtos[i].qtd <= 0)
+      {
+        printf(" (Esgotado)");
+      }
+
+      printf("\n");
+    }
+  }
+
+  //Escolha do Pão
+  while(1)
+  {
+    scanf(" %d", &escolha);
+
+    for(int i = 0; i < tam; i++)
+    {
+      if(produtos[i].tipo == 2)
+      {
+        if(escolha == produtos[i].id)
+        {
+          if(produtos[i].qtd > 0)
+          {
+            sanduiche.pao = produtos[i];
+            ok = 1;
+            break;
+          }
+          else
+          {
+            esgotado = 1;
+          }
+        }
+      }
+    }
+
+    if(ok == 0)
+    {
+      if(esgotado == 1)
+      {
+        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+        esgotado = 0;
+      }
+      else
+      {
+        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+      }
+    }
+    else if(ok == 1)
+    {
+      ok = 0;
+      break;
+    }
+  }
+
+  printf("\t\n\nQUARTO PASSO:\n");
+  printf("Escolha o tipo de molho do seu sanduíche!");
+
+  //Impressão das Opções de Molho
+  for(int i = 0; i < tam; i++)
+  {
+    if(produtos[i].tipo == 3)
+    { 
+      printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+      if(produtos[i].qtd <= 0)
+      {
+        printf(" (Esgotado)");
+      }
+
+      printf("\n");
+    }
+  }
+
+  //Escolha do Molho
+  while(1)
+  {
+    scanf(" %d", &escolha);
+
+    for(int i = 0; i < tam; i++)
+    {
+      if(produtos[i].tipo == 3)
+      {
+        if(escolha == produtos[i].id)
+        {
+          if(produtos[i].qtd > 0)
+          {
+            sanduiche.molho = produtos[i];
+            ok = 1;
+            break;
+          }
+          else
+          {
+            esgotado = 1;
+          }
+        }
+      }
+    }
+
+    if(ok == 0)
+    {
+      if(esgotado == 1)
+      {
+        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+        esgotado = 0;
+      }
+      else
+      {
+        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+      }
     }
     else if(ok == 1)
     {
