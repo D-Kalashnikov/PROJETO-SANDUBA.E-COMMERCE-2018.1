b01cc89a458f9c3bc95fc71485e8c2b1661f565c
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-08 16:22:00 -0300

    Create bestseller-function.c

---
+++ bestseller-function.c
@@ -0,0 +1,31 @@
+void bestseller(){
+  readStorageDB("storageDB.txt", produtos, ind_tprodutos);
+
+  int aux_v=0;
+  int aux_id=0;
+  char aux_string[30];
+
+  for(int i = 0; i < ind_tprodutos; i++){
+    for (int j = i + 1; j < ind_tprodutos; j++) {
+			if (produtos[i].vendidos < produtos[j].vendidos) {
+				//
+        aux_v = produtos[i].vendidos;
+				produtos[i].vendidos = produtos[j].vendidos;
+				produtos[j].vendidos = aux_v;
+        //
+        aux_id = produtos[i].id;
+        produtos[i].id = produtos[j].id;
+        produtos[j].id = aux_id;
+        //
+        strcpy(aux_string, produtos[i].nome);
+        strcpy(produtos[i].nome, produtos[j].nome);
+        strcpy(produtos[j].nome, aux_string);
+			}
+		}
+  }
+
+  for(i = 0; i < ind_tprodutos; i++){
+    printf("Produtos mais vendidos:\n");
+    printf("ID: %i\tNome:%s\tItens vendidos:%i\n",produtos[i].id,produtos[i].nome, produtos[i].vendidos);
+  }
+}
