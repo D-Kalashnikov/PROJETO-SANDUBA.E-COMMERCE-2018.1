aed7c414073c5b4b033ae2c1845ec77c12570e8a
RavenbornIV <41174251+RavenbornIV@users.noreply.github.com>
2018-08-13 20:40:25 -0300

    Update compra_sanduiche.c
    Essa update foi uma baguncinha, e a principal parte foi criar a lógica da confirmação (se o cliente quer adicionar o sanduíche ao carrinho ou não), de modo que ele possa repetir o processo de seleção quantas vezes quiser até resolver que o sanduíche que escolheu é o certo. Ainda falta ver aquela parte do vetor de sanduíches pra o caso do cliente querer comprar vários de uma vez, então talvez eu precise modificar algumas coisinhas nessa parte de repetição...enfim, por enquanto é isso.

--- compra_sanduiche.c
+++ compra_sanduiche.c
@@ -1,378 +1,478 @@
-void compra_sanduiche(int* quant, Produto produtos[])
+float compra_sanduiche(int* quant, Produto produtos[])
 {
-  int ok = 0, escolha, esgotado = 0, num = 10;
-  float somatorio;
+  int compra = 1, ok = 0, escolha, esgotado = 0, num = 10;
+  float somatorio = 0;
   Sanduiche sanduiche;
 
-  printf("\tPRIMEIRO PASSO:\n");
-  printf("Escolha o tamanho do seu sanduíche!\n\n1 - 15cm\n2 - 30cm\n\n");
-
-  //Escolha do Tamanho
-  while(1)
+  while(compra == 1)
   {
-    scanf(" %d", &escolha);
+    printf("\tPRIMEIRO PASSO:\n");
+    printf("Escolha o tamanho do seu sanduíche!\n\n1 - 15cm\n2 - 30cm\n\n");
 
-    if(escolha == 1)
+    //Escolha do Tamanho
+    while(1)
     {
-      sanduiche.tamanhopao = 15;
-      break;
-    }
-    else if(escolha == 2)
-    {
-      sanduiche.tamanhopao = 30;
-      break;
-    }
-    else
-    {
-      printf("\nComando inválido, por favor insira um dos números indicados.\n\n");
-    }
-  }
+      scanf(" %d", &escolha);
 
-  printf("\t\n\nSEGUNDO PASSO:\n");
-  printf("Escolha o tipo de pão do seu sanduíche!");
-
-  //Impressão das Opções de Pão
-  for(int i = 0; i < num; i++)
-  {
-    if(produtos[i].tipo == 1)
-    { 
-      printf("%d - %s", produtos[i].id, produtos[i].nome);
-
-      if(produtos[i].qtd <= 0)
+      if(escolha == 1)
       {
-        printf(" (Esgotado)");
+        sanduiche.tamanhopao = 15;
+        break;
       }
-
-      printf("\n");
+      else if(escolha == 2)
+      {
+        sanduiche.tamanhopao = 30;
+        break;
+      }
+      else
+      {
+        printf("\nComando inválido, por favor insira um dos números indicados.\n\n");
+      }
     }
-  }
 
-  //Escolha do Pão
-  while(1)
-  {
-    scanf(" %d", &escolha);
+    printf("\t\n\nSEGUNDO PASSO:\n");
+    printf("Escolha o tipo de pão do seu sanduíche!");
 
+    //Impressão das Opções de Pão
     for(int i = 0; i < num; i++)
     {
       if(produtos[i].tipo == 1)
-      {
-        if(escolha == produtos[i].id)
+      { 
+        printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+        if(produtos[i].qtd <= 0)
         {
-          if(produtos[i].qtd > 0)
+          printf(" (Esgotado)");
+        }
+
+        printf("\n");
+      }
+    }
+
+    //Escolha do Pão
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      for(int i = 0; i < num; i++)
+      {
+        if(produtos[i].tipo == 1)
+        {
+          if(escolha == produtos[i].id)
           {
-            sanduiche.pao = produtos[i];
-            ok = 1;
-            break;
-          }
-          else
-          {
-            esgotado = 1;
+            if(produtos[i].qtd > 0)
+            {
+              sanduiche.pao = produtos[i];
+              ok = 1;
+              break;
+            }
+            else
+            {
+              esgotado = 1;
+            }
           }
         }
       }
-    }
 
-    if(ok == 0)
-    {
-      if(esgotado == 1)
+      if(ok == 0)
       {
-        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
-        esgotado = 0;
+        if(esgotado == 1)
+        {
+          printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+          esgotado = 0;
+        }
+        else
+        {
+          printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        }
       }
-      else
+      else if(ok == 1)
       {
-        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        ok = 0;
+        break;
       }
     }
-    else if(ok == 1)
-    {
-      ok = 0;
-      break;
-    }
-  }
 
-  printf("\t\n\nTERCEIRO PASSO:\n");
-  printf("Escolha o recheio do seu sanduíche!");
+    printf("\t\n\nTERCEIRO PASSO:\n");
+    printf("Escolha o recheio do seu sanduíche!");
 
-  //Impressão das Opções de Recheio
-  for(int i = 0; i < num; i++)
-  {
-    if(produtos[i].tipo == 2)
-    { 
-      printf("%d - %s", produtos[i].id, produtos[i].nome);
-
-      if(produtos[i].qtd <= 0)
-      {
-        printf(" (Esgotado)");
-      }
-
-      printf("\n");
-    }
-  }
-
-  //Escolha do Recheio
-  while(1)
-  {
-    scanf(" %d", &escolha);
-
+    //Impressão das Opções de Recheio
     for(int i = 0; i < num; i++)
     {
       if(produtos[i].tipo == 2)
-      {
-        if(escolha == produtos[i].id)
+      { 
+        printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+        if(produtos[i].qtd <= 0)
         {
-          if(produtos[i].qtd > 0)
+          printf(" (Esgotado)");
+        }
+
+        printf("\n");
+      }
+    }
+
+    //Escolha do Recheio
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      for(int i = 0; i < num; i++)
+      {
+        if(produtos[i].tipo == 2)
+        {
+          if(escolha == produtos[i].id)
           {
-            sanduiche.recheio = produtos[i];
-            ok = 1;
-            break;
-          }
-          else
-          {
-            esgotado = 1;
+            if(produtos[i].qtd > 0)
+            {
+              sanduiche.recheio = produtos[i];
+              ok = 1;
+              break;
+            }
+            else
+            {
+              esgotado = 1;
+            }
           }
         }
       }
-    }
 
-    if(ok == 0)
-    {
-      if(esgotado == 1)
+      if(ok == 0)
       {
-        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
-        esgotado = 0;
+        if(esgotado == 1)
+        {
+          printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+          esgotado = 0;
+        }
+        else
+        {
+          printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        }
       }
-      else
+      else if(ok == 1)
       {
-        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        ok = 0;
+        break;
       }
     }
-    else if(ok == 1)
-    {
-      ok = 0;
-      break;
-    }
-  }
 
-  printf("\t\n\nQUARTO PASSO:\n");
-  printf("Escolha o tipo de queijo do seu sanduíche!");
+    printf("\t\n\nQUARTO PASSO:\n");
+    printf("Escolha o tipo de queijo do seu sanduíche!");
 
-  //Impressão das Opções de Queijo
-  for(int i = 0; i < num; i++)
-  {
-    if(produtos[i].tipo == 3)
-    { 
-      printf("%d - %s", produtos[i].id, produtos[i].nome);
-
-      if(produtos[i].qtd <= 0)
-      {
-        printf(" (Esgotado)");
-      }
-
-      printf("\n");
-    }
-  }
-
-  //Escolha do Queijo
-  while(1)
-  {
-    scanf(" %d", &escolha);
-
+    //Impressão das Opções de Queijo
     for(int i = 0; i < num; i++)
     {
       if(produtos[i].tipo == 3)
-      {
-        if(escolha == produtos[i].id)
+      { 
+        printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+        if(produtos[i].qtd <= 0)
         {
-          if(produtos[i].qtd > 0)
+          printf(" (Esgotado)");
+        }
+
+        printf("\n");
+      }
+    }
+
+    //Escolha do Queijo
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      for(int i = 0; i < num; i++)
+      {
+        if(produtos[i].tipo == 3)
+        {
+          if(escolha == produtos[i].id)
           {
-            sanduiche.queijos = produtos[i];
-            ok = 1;
-            break;
-          }
-          else
-          {
-            esgotado = 1;
+            if(produtos[i].qtd > 0)
+            {
+              sanduiche.queijos = produtos[i];
+              ok = 1;
+              break;
+            }
+            else
+            {
+              esgotado = 1;
+            }
           }
         }
       }
-    }
 
-    if(ok == 0)
-    {
-      if(esgotado == 1)
+      if(ok == 0)
       {
-        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
-        esgotado = 0;
+        if(esgotado == 1)
+        {
+          printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+          esgotado = 0;
+        }
+        else
+        {
+          printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        }
       }
-      else
+      else if(ok == 1)
       {
-        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        ok = 0;
+        break;
       }
     }
-    else if(ok == 1)
-    {
-      ok = 0;
-      break;
-    }
-  }
 
-  printf("\t\n\nQUINTO PASSO:\n");
-  printf("Escolha o tipo de molho do seu sanduíche!");
+    printf("\t\n\nQUINTO PASSO:\n");
+    printf("Escolha o tipo de molho do seu sanduíche!");
 
-  //Impressão das Opções de Molho
-  for(int i = 0; i < num; i++)
-  {
-    if(produtos[i].tipo == 4)
-    { 
-      printf("%d - %s", produtos[i].id, produtos[i].nome);
-
-      if(produtos[i].qtd <= 0)
-      {
-        printf(" (Esgotado)");
-      }
-
-      printf("\n");
-    }
-  }
-
-  //Escolha do Molho
-  while(1)
-  {
-    scanf(" %d", &escolha);
-
+    //Impressão das Opções de Molho
     for(int i = 0; i < num; i++)
     {
       if(produtos[i].tipo == 4)
-      {
-        if(escolha == produtos[i].id)
+      { 
+        printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+        if(produtos[i].qtd <= 0)
         {
-          if(produtos[i].qtd > 0)
+          printf(" (Esgotado)");
+        }
+
+        printf("\n");
+      }
+    }
+
+    //Escolha do Molho
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      for(int i = 0; i < num; i++)
+      {
+        if(produtos[i].tipo == 4)
+        {
+          if(escolha == produtos[i].id)
           {
-            sanduiche.molho = produtos[i];
-            ok = 1;
+            if(produtos[i].qtd > 0)
+            {
+              sanduiche.molho = produtos[i];
+              ok = 1;
+              break;
+            }
+            else
+            {
+              esgotado = 1;
+            }
+          }
+        }
+      }
+
+      if(ok == 0)
+      {
+        if(esgotado == 1)
+        {
+          printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+          esgotado = 0;
+        }
+        else
+        {
+          printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        }
+      }
+      else if(ok == 1)
+      {
+        ok = 0;
+        break;
+      }
+    }
+
+    printf("\t\n\nSEXTO PASSO:\n");
+    printf("Escolha as verduras que colocará em seu sanduíche (no máximo 3 tipos)!");
+
+    //Impressão das Opções de Verdura
+    for(int i = 0; i < num; i++)
+    {
+      if(produtos[i].tipo == 5)
+      { 
+        printf("%d - %s", produtos[i].id, produtos[i].nome);
+
+        if(produtos[i].qtd <= 0)
+        {
+          printf(" (Esgotado)");
+        }
+
+        printf("\n");
+      }
+    }
+
+    //Escolha das Verduras
+
+    int verduras = 0, continuar = 0;
+
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      for(int i = 0; i < num; i++)
+      {
+        if(produtos[i].tipo == 5)
+        {
+          if(escolha == produtos[i].id)
+          {
+            if(produtos[i].qtd > 0)
+            {
+              sanduiche.salada[verduras] = produtos[i];
+              verduras = verduras + 1;
+
+              if((3 - verduras) > 0)
+              {
+                printf("\nDeseja pôr mais alguma verdura? (Você ainda pode colocar mais %d!\n\n)", (3 - verduras));
+                printf("1 - Sim\n2 - Não\n\n");
+                while(1)
+                {
+                  scanf(" %d", &escolha);
+
+                  if(escolha == 1)
+                  {
+                    continuar = 1;
+                    break;
+                  }
+                  else if(escolha == 2)
+                  {
+                    ok = 1;
+                    break;
+                  }
+                  else
+                  {
+                    printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+                  }
+                }
+              }
+            }
+            else
+            {
+              esgotado = 1;
+            }
+          }
+        }
+      }
+
+      if(ok == 0)
+      {
+        if(continuar == 1)
+        {
+          continuar = 0;
+          printf("\nEscolha a próxima verdura!\n\n");
+        }
+        else if(esgotado == 1)
+        {
+          printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
+          esgotado = 0;
+        }
+        else
+        {
+          printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
+        }
+      }
+      else if(ok == 1)
+      {
+        ok = 0;
+        break;
+      }
+    }
+
+    if(sanduiche.tamanhopao == 15)
+    {
+      somatorio = somatorio + (sanduiche.recheio.valorv + sanduiche.molho.valorv + sanduiche.queijos.valorv + sanduiche.pao.valorv);
+    }
+    else if(sanduiche.tamanhopao == 30)
+    {
+      somatorio = somatorio + ((sanduiche.recheio.valorv + sanduiche.molho.valorv + sanduiche.queijos.valorv + sanduiche.pao.valorv) * 1.8);
+    }
+
+    printf("\nConferindo:\n\n");
+
+    printf("Sanduíche de %s\n Pão: %s\nQueijo: %s\nMolho: %s\nVegetais: ", sanduiche.recheio.nome, sanduiche.pao.nome, sanduiche.queijos.nome, sanduiche.molho.nome);
+    for(int i = 0; i < verduras; i++)
+    {
+      if((verduras - i) > 1)
+      {
+      printf("%s, ", sanduiche.salada[i].nome);
+      }
+      else
+      {
+        printf("%s", sanduiche.salada[i].nome);
+      }
+    }
+    printf("\nPreço: R$ %f", somatorio);
+
+    printf("\n\nDeseja adicionar esse Sanduíche ao Carrinho?\n\n1 - Sim\n2 - Não");
+
+    while(1)
+    {
+      scanf(" %d", &escolha);
+
+      if(escolha == 1)
+      {
+        compra = 0;
+        break;
+      }
+      else if(escolha == 2)
+      {
+        int certeza;
+
+        printf("\n\nTem certeza? Isso irá desfazer as escolhas pro sanduíche até então!\n\n1 - Sim\n2 - Não");
+        
+        while(1)
+        {
+          scanf(" %d", &escolha);
+
+          if(escolha == 1)
+          {
+            certeza = 1;
+            break;
+          }
+          else if(escolha == 2)
+          {
+            certeza = 0;
             break;
           }
           else
           {
-            esgotado = 1;
+            printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
           }
         }
-      }
-    }
 
-    if(ok == 0)
-    {
-      if(esgotado == 1)
-      {
-        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
-        esgotado = 0;
-      }
-      else
-      {
-        printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
-      }
-    }
-    else if(ok == 1)
-    {
-      ok = 0;
-      break;
-    }
-  }
-
-  printf("\t\n\nSEXTO PASSO:\n");
-  printf("Escolha as verduras que colocará em seu sanduíche (no máximo 3 tipos)!");
-
-  //Impressão das Opções de Verdura
-  for(int i = 0; i < num; i++)
-  {
-    if(produtos[i].tipo == 5)
-    { 
-      printf("%d - %s", produtos[i].id, produtos[i].nome);
-
-      if(produtos[i].qtd <= 0)
-      {
-        printf(" (Esgotado)");
-      }
-
-      printf("\n");
-    }
-  }
-
-  //Escolha das Verduras
-
-  int verduras = 0, continuar = 0;
-
-  while(1)
-  {
-    scanf(" %d", &escolha);
-
-    for(int i = 0; i < num; i++)
-    {
-      if(produtos[i].tipo == 5)
-      {
-        if(escolha == produtos[i].id)
+        if(certeza == 1)
         {
-          if(produtos[i].qtd > 0)
+          printf("\nDeseja...\n\n1 - Refazer o seu sanduíche.\n2 - Finalizar a compra assim mesmo.");
+
+          while(1)
           {
-            sanduiche.salada[verduras] = produtos[i];
-            verduras = verduras + 1;
+            scanf(" %d", &escolha);
 
-            if((3 - verduras) > 0)
+            if(escolha == 1)
             {
-              printf("\nDeseja pôr mais alguma verdura? (Você ainda pode colocar mais %d!\n\n)", (3 - verduras));
-              printf("1 - Sim\n2 - Não\n\n");
-              while(1)
-              {
-                scanf(" %d", &escolha);
-
-                if(escolha == 1)
-                {
-                  continuar = 1;
-                  break;
-                }
-                else if(escolha == 2)
-                {
-                  ok = 1;
-                  break;
-                }
-                else
-                {
-                  printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
-                }
-              }
+              break;
+            }
+            else if(escolha == 2)
+            {
+              printf("\n\nAgradecemos pela preferência!\n");
+              return 0;
+            }
+            else
+            {
+              printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
             }
           }
-          else
-          {
-            esgotado = 1;
-          }
         }
-      }
-    }
-
-    if(ok == 0)
-    {
-      if(continuar == 1)
-      {
-        continuar = 0;
-        printf("\nEscolha a próxima verdura!\n\n");
-      }
-      else if(esgotado == 1)
-      {
-        printf("\nProduto esgotado. Por favor, escolha outra opção.\n\n");
-        esgotado = 0;
+        else if(certeza == 0)
+        {
+          compra = 0;
+          break;
+        }
       }
       else
       {
         printf("\nComando inválido. Por favor, insira um dos números indicados.\n\n");
       }
     }
-    else if(ok == 1)
-    {
-      ok = 0;
-      break;
-    }
   }
-
+  
 }
