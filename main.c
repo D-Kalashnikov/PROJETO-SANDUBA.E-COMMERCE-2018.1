a4121451936638df4e0af8e9e1ba00214c0fe422
josemarassuncao <42063546+josemarassuncao@users.noreply.github.com>
2018-08-15 17:49:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,15 @@ void main()
 {
   Produto produto[100];
   Funcionario funcionario[30];
-  int qte_prod_cad = 0;
+  int qte_prod_cad = 0, indice_func = 0;
   float receita = 10000;// 10.000 investimento inicial
-  float despesa = 500 + 500 + 2000; // manut. site + frete + salários
+  float despesa = 500 + 500; // manut. site + frete
   float saldo_bancario = receita - despesa;
   float lucro=0; 
   
   gerenciar_produto(produto, 100, &qte_prod_cad, &saldo_bancario);
-  calculo_rel_fin (produto, qte_prod_cad, &receita, &despesa, &lucro);
-  impressao_rel_financeiro(produto, qte_prod_cad, receita, despesa, lucro);
+  calculo_rel_fin (produto, qte_prod_cad, funcionario, indice_func, &receita, &despesa, &lucro);
+  impressao_rel_financeiro(produto, qte_prod_cad, funcionario, indice_func, receita, despesa, lucro);
   
   
 }
