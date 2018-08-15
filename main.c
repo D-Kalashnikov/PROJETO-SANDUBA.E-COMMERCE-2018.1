843f296345059b489081a1cba703d69f11028146
josemarassuncao <42063546+josemarassuncao@users.noreply.github.com>
2018-08-15 17:13:42 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+void main()
+{
+  Produto produto[100]; 
+  int qte_prod_cad = 0;
+  float receita = 10000;// 10.000 investimento inicial
+  float despesa = 500 + 500 + 2000; // manut. site + frete + salários
+  float saldo_bancario = receita - despesa;
+  float lucro=0; 
+  
+  gerenciar_produto(produto, 100, &qte_prod_cad, &saldo_bancario);
+  calculo_rel_fin (produto, qte_prod_cad, &receita, &despesa, &lucro);
+  impressao_rel_financeiro(produto, qte_prod_cad, receita, despesa, lucro);
+  
+  
+}
