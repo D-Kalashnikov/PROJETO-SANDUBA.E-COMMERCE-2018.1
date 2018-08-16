e1b137594708b94b4d574f1a14c1546a420306b3
josemarassuncao <42063546+josemarassuncao@users.noreply.github.com>
2018-08-16 20:21:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ void main()
   float receita = 10000;// 10.000 investimento inicial
   float despesa = 500 + 500; // manut. site + frete
   float saldo_bancario = receita - despesa;
-  float lucro=0; 
+  float lucro=0;
+  Cliente cliente;
   
   gerenciar_produto(produto, 100, &qte_prod_cad, &saldo_bancario);
   calculo_rel_fin (produto, qte_prod_cad, funcionario, indice_func, &receita, &despesa, &lucro);
   impressao_rel_financeiro(produto, qte_prod_cad, funcionario, indice_func, receita, despesa, lucro);
+  compra_sanduiche(&qte_prod_cad, produto, qte_prod_cad, &cliente);
   
   
 }
