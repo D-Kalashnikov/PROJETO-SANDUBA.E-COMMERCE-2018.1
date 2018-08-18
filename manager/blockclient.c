dd62b7bb5c7d0d86c9e4ec61d602e3c76dde536f
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-18 12:48:49 -0300

    Create blockclient.c

---
+++ manager/blockclient.c
@@ -0,0 +1,31 @@
+void BlockClient(int quant, Cliente clientes[quant]){
+  int choice;
+  int search = client_search(quant, clientes);
+  int flag = 0;
+
+  do{
+  if(search != -1){
+    printf("Deseja realmente bloquear esse usuario?\n");
+    printf("1 - Sim\n");
+    printf("2 - Não\n");
+    scanf("%i", &choice);
+      if(choice == 1){
+        clientes[cont].dados.status = 3;
+        flag = 1;
+      }
+
+      else if(choice == 2){
+        printf("Operação cancelada\nAperte uma tecla para sair...");
+        flag = 1;
+        getchar();
+        system("cls");
+      }
+
+      else{
+        printf("Comando inválido... Tente novamente\n");
+        getchar();
+        system("cls");
+      }
+    }
+  }while(flag == 0);
+}
