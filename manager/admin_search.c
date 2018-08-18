19fbc75390ad961e907cdc75064e7cc2542e0f98
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-18 20:52:16 -0300

    Update admin_search.c

--- manager/admin_search.c
+++ manager/admin_search.c
@@ -1,21 +1,11 @@
-int admin_search(int quant, Admin admins[quant])
+int admin_search(int ind_admins, Admin admins[ind_admins], char cpf){
+  int i;
+  for(i=0; i<ind_admins; i++)
   {
-  int cont;
-  char cpf[13];
-
-  printf("Digite o CPF a ser buscado:\n");
-  scanf("%s", cpf);
-
-  for(cont=0; cont<quant; cont++)
-  {
-    if(strcmp(cpf, admins[cont].dados.cpf)== 0)
+    if(strcmp(cpf, admins[i].dados.cpf)== 0)
     {
-      printf("Admin encontrado\n");
-      printf("Nome: %s\nIdade: %i\nCPF: %s\nStatus: %i\n", admins[cont].dados.nome, admins[cont].dados.idade, admins[cont].dados.cpf, admins[cont].dados.status);
-
-      return cont;
+      return i;
     }
   }
-  printf("Admin não encontrado.\n");
   return -1;
 }
