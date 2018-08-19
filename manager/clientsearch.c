e5f04d3da77cab2b5504d76df48a5eab7e13c32f
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-18 21:01:10 -0300

    Update clientsearch.c

--- manager/clientsearch.c
+++ manager/clientsearch.c
@@ -1,19 +1,19 @@
-int client_search(int quant, Cliente clientes[quant])
+int client_search(int ind_tclientes, Cliente clientes[ind_tclientes])
   {
-  int cont;
+  int i;
   char cpf[13];
 
   printf("Digite o CPF a ser buscado:\n");
   scanf("%s", cpf);
 
-  for(cont=0; cont<quant; cont++)
+  for(i=0; i<ind_tclientes; i++)
   {
-    if(strcmp(cpf, clientes[cont].dados.cpf)== 0)
+    if(strcmp(cpf, clientes[i].dados.cpf)== 0)
     {
       printf("Cliente encontrado\n");
-      printf("Nome: %s\nIdade: %i\nCPF: %s\nStatus: %i\n", clientes[cont].dados.nome, clientes[cont].dados.idade, clientes[cont].dados.cpf, clientes[cont].dados.status);
+      printf("Nome: %s\nIdade: %i\nCPF: %s\nStatus: %i\n", clientes[i].dados.nome, clientes[i].dados.idade, clientes[i].dados.cpf, clientes[i].dados.status);
 
-      return cont;
+      return i;
     }
   }
   printf("Cliente não encontrado.\n");
