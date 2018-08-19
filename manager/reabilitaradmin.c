5b062c7fa7e0d5a5031fa2950ebd103bcec522a1
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-18 21:15:01 -0300

    Update reabilitaradmin.c

--- manager/reabilitaradmin.c
+++ manager/reabilitaradmin.c
@@ -1,16 +1,20 @@
-void AdminDisable(int quant, Admin admins[quant]){
+void AdminEnabled(int ind_admins, Admin admins[ind_admins]){
   int choice;
+  char cpf[13];
+  printf("Digite o CPF do Admin a ser reabilitado:\n");
   int search = adm_search(quant, admins);
   int flag = 0;
 
   do{
   if(search != -1){
+    printf("Admin encontrado\n");
+    printf("Nome: %s\nIdade: %i\nCPF: %s\nStatus: %i\n", admins[search].dados.nome, admins[search].dados.idade, admins[search].dados.cpf, admins[search].dados.status);
     printf("Deseja realmente reabilitar esse administrador?\n");
     printf("1 - Sim\n");
     printf("2 - Não\n");
     scanf("%i", &choice);
       if(choice == 1){
-        admins[cont].dados.status = 4;
+        admins[search].dados.status = 4;
         flag = 1;
       }
 
@@ -25,6 +29,10 @@ void AdminDisable(int quant, Admin admins[quant]){
         printf("Comando inválido... Tente novamente\n");
         getchar();
         system("cls");
+      }
+    
+    else{
+      printf("Admin não encontrado!\n");
       }
     }
   }while(flag == 0);
