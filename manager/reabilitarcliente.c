aeb4f706bf77bd33f70371d4da435a8bd7538950
D-Kalashnikov <41235143+D-Kalashnikov@users.noreply.github.com>
2018-08-18 23:25:43 -0300

    Update reabilitarcliente.c

--- manager/reabilitarcliente.c
+++ manager/reabilitarcliente.c
@@ -10,8 +10,15 @@ void DisblockClient(int ind_tclientes, Cliente clientes[ind_tclientes]){
     printf("2 - Não\n");
     scanf("%i", &choice);
       if(choice == 1){
+        if(clientes[search].dados.status != 1){
         clientes[search].dados.status = 1;
         flag = 1;
+        }
+        
+        else{
+          printf("Usuario já esta disponível!\n");
+          flag = 1;
+        }
       }
 
       else if(choice == 2){
