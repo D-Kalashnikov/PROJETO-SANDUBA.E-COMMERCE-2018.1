bea69764fa9899da3964ebf586cdcaf97300c7b7
josemarassuncao <42063546+josemarassuncao@users.noreply.github.com>
2018-08-19 23:18:45 -0300

    Update relatorio_financeiro.c

--- josemar/relatorio_financeiro.c
+++ josemar/relatorio_financeiro.c
@@ -1,60 +1,50 @@
-void impressao_rel_financeiro(Produto produto[], int qte_prod_cad, Funcionario funcionario[], int indice_func, float receita, float despesa, float lucro)
+void calculo_e_impressao_rel_fin (Produto produto[],int qte_prod_cad, float saldo_bancario,float  percentual_sal, float percentual_odesp)
 {
+
+  //Cálculo do relatório financeiro.
+  float receita = 0, despesa = 0, despesa_apagar = 0;
   
-  printf("(+) RECEITA:         = %.2f\n", receita);
-  printf("Invest. inicial = 10000.00\n");
   for(int i=0; i<qte_prod_cad;i++)
   {
-    printf("Venda de produtos:\n");
+   receita += produto[i].totalv;
+  }
+
+  for(int i=0; i<qte_prod_cad;i++)
+  {
+   despesa += produto[i].totalv * (percentual_odesp + percentual_sal);
+   despesa_apagar += produto[i].totalv * (percentual_odesp + percentual_sal);
+
+   despesa += produto[i].totalc;
+  }
+  //impressao do relatório financeiro
+
+  printf("\n(+)RECEITA:         = %.2f\n", receita);
+  
+  printf("Venda de produtos:\n");
+
+  for(int i=0; i<qte_prod_cad;i++)
+  {
     printf("%s = ",produto[i].nome);
     printf("%.2f\n",produto[i].totalv);
   }
+
   printf("\n\n");
   printf("(-) DESPESA:          = %.2f\n", despesa);
-  printf("Manut. do site =  500.00\n");
-  printf("Frete =  500.00\n");
-
-  for(int i=0; i<indice_func;i++)
-  {
-    printf("Salário funcionarios:\n");
-    printf("%s = ",funcionario[i].dados.nome);
-    printf("%.2f\n",funcionario[i].salario);
-  }
+  printf("Manut. do site e Frete= %.2f\n", receita * percentual_odesp);
+  printf("Salários              = %.2f\n", receita * percentual_sal);
   printf("\n\n");
-  
+  printf("Compra de produtos:\n");
+
   for(int i=0; i<qte_prod_cad;i++)
   {
-    printf("Compra de produtos:\n");
     printf("%s = ",produto[i].nome);
     printf("%.2f\n",produto[i].totalc);
   }
+
   printf("\n\n");
-  printf("(=) LUCRO:            = %.2f\n", lucro);
+  printf("(=) LUCRO:            =     %.2f\n\n", receita - despesa);
+  printf("(+)Invest. inicial       = 10000.00\n\n");
+  printf("(=) SALDO BANCÁRIO:   =     %.2f\n", saldo_bancario - despesa_apagar);
   printf("\n");
-
-
-}
-  void calculo_rel_fin(Produto produto[], int qte_prod_cad, Funcionario funcionario[], int indice_func, float* preceita, float* pdespesa, float* plucro)
-{
-  
-  
-  for(int i=0; i<qte_prod_cad;i++)
-  {
-    *preceita += produto[i].totalv;
-  }
-
-  for(int i=0; i<qte_prod_cad;i++)
-  {
-    *pdespesa += produto[i].totalc;
-  }
-
-  for(int i=0; i<indice_func;i++)
-  {
-    *pdespesa += funcionario[i].salario;
-  }
-
-
-
-  *plucro = *preceita - *pdespesa;
   
 }
