12dd8e0a974fe3070a01664400978bb2ca2f0941
josemarassuncao <42063546+josemarassuncao@users.noreply.github.com>
2018-08-20 22:32:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ void main()
   int qte_prod_cad = 0, indice_func = 0, indice_cliente = 0;
   float saldo_bancario = 10000, percentual_sal = 0.01, percentual_odesp = 0.001;
   Cliente cliente;
+  int pos_cliente;
   
   gerenciar_produto(produto, 100, &qte_prod_cad, &saldo_bancario);
   compra_sanduiche(produto, qte_prod_cad, &cliente, &saldo_bancario);
